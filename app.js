8a2c3b90499c924c1d73b587e851d4c18a24d1b8
GosuPaper <florian.girardin@mines-ales.org>
2022-02-15 17:10:09 +0100

    debut 1ere requete

--- app.js
+++ app.js
@@ -7,19 +7,25 @@ const connectionMYSQL = mysql.createConnection({
     database: 'tpbdd'
 });
 
-const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'neo4j'));
+const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'root'), {
+    maxConnectionLifetime: 3 * 60 * 60 * 1000, // 3 hours
+    maxConnectionPoolSize: 50,
+    connectionAcquisitionTimeout: 2 * 60 * 1000, // 120 seconds
+    disableLosslessIntegers: true
+});
+
 connectionMYSQL.connect((err) => {
     if (err) {
         throw err;
     }
 });
-const session = driver.session();
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-let mode = 'SGBDR';
+let mode = 'MySQL';
 
 function formatResponse(resultObj) {
     const result = [];
@@ -33,7 +39,8 @@ function formatResponse(resultObj) {
 
 async function executeCypherQuery(statement, params = {}) {
     try {
-        const result = session.run(statement, params);
+        const session = driver.session();
+        const result = await session.run(statement, params);
         await session.close();
         return result;
     } catch (error) {
@@ -60,28 +67,33 @@ app.get('/Base', (req, res) => {
 })
 
 app.post('/RequestSelect', async (req, res) => {
-    if(mode === "SGBDR")
+    if(mode === "MySQL")
         await requestMySQL('SELECT * FROM ' + req.body.table + ';', res);
     else if(mode === "NoSQL") {
-        const query = `MATCH (n:Movie) RETURN n LIMIT 25`;
+        const query = `MATCH (n:Person) RETURN n LIMIT 25`;
         const params = {
         };
-        let pre_query = new Date().getTime();
-        const resultObj = await executeCypherQuery(query, params);
-        let post_query = new Date().getTime();
-        // calculate the duration in seconds
-        let duration = (post_query - pre_query) / 1000;
-        const result = formatResponse(resultObj);
-        res.render('index.html', {mode: 'NoSQL', time: duration, requestResult: result});
-
+        await requestNoSQL(query, params, res);
     }
         //await requestNoSQL('SELECT * FROM ' + req.body.table + ';', res);
 })
 
+app.post('/ProductListFollowers', async (req, res) => {
+    if(mode === "MySQL")
+        await requestMySQL('SELECT * FROM ' + req.body.table + ';', res);
+    else if(mode === "NoSQL") {
+        const query = `MATCH (n:Person) RETURN n LIMIT 25`;
+        const params = {
+        };
+        await requestNoSQL(query, params, res);
+    }
+    //await requestNoSQL('SELECT * FROM ' + req.body.table + ';', res);
+})
+
 app.post('/SetMode', (req, res) => {
-    if (req.body.mode === 'SGBDR') {
-        mode = "SGBDR";
-        res.render('index.html', {mode: 'SGBDR', time: '0', requestResult: ''});
+    if (req.body.mode === 'MySQL') {
+        mode = "MySQL";
+        res.render('index.html', {mode: 'MySQL', time: '0', requestResult: ''});
     } else if (req.body.mode === 'NoSQL') {
         mode = "NoSQL";
         res.render('index.html', {mode: 'NoSQL', time: '0', requestResult: ''});
@@ -96,17 +108,23 @@ const requestMySQL = async function (query, res) {
     let pre_query = new Date().getTime();
     await connectionMYSQL.query(query, function (err, result, fields) {
         if (err) {
-            res.render('index.html', {mode: 'SGBDR', time: '0', requestResult: err});
+            res.render('index.html', {mode: 'MySQL', time: '0', requestResult: err});
             throw err;
         }
         let post_query = new Date().getTime();
         // calculate the duration in seconds
         let duration = (post_query - pre_query) / 1000;
         console.log(result);
-        res.render('index.html', {mode: 'SGBDR', time: duration, requestResult: result});
+        res.render('index.html', {mode: 'MySQL', time: duration, requestResult: result});
     });
 }
 
-const requestNoSQL = async function (query, res) {
-    //TODO
+const requestNoSQL = async function (query, params, res) {
+    let pre_query = new Date().getTime();
+    const resultObj = await executeCypherQuery(query, params);
+    let post_query = new Date().getTime();
+    // calculate the duration in seconds
+    let duration = (post_query - pre_query) / 1000;
+    const result = formatResponse(resultObj);
+    res.render('index.html', {mode: 'NoSQL', time: duration, requestResult: result});
 }
