fe3b0a566a7e0ea626cc7a186dc956ef2bc5b4a1
GosuPaper <florian.girardin@mines-ales.org>
2022-03-04 18:46:24 +0100

    super rapid insert 2000

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/RequestSelect', async (req, res) => {
     if (mode === "MySQL")
         await sqlManager.requestMySQL('SELECT * FROM ' + req.body.table + ';', res);
     else if (mode === "NoSQL") {
-        const query = `MATCH (n:Person) RETURN n LIMIT 25`;
+        const query = `MATCH (n:`+ req.body.table + `) RETURN n LIMIT 25`;
         const params = {};
         await nosqlManager.requestNoSQL(query, params, res);
     }
@@ -40,7 +40,7 @@ app.post('/ProductListFollowers', async (req, res) => {
         await sqlManager.requestMySQL('WITH recursive tree(node, depth) AS ( SELECT link.idFollower AS node, 1 AS depth FROM link WHERE link.idInflu = ' + req.body.userId + ' UNION ALL SELECT link.idFollower, depth + 1 FROM link JOIN tree ON tree.node = link.idInflu where depth <= ' + req.body.niveau + ') SELECT product.id AS productId, product.ref AS product, count(*) as nbAchatsfrom from product join buy on product.id = buy.idProduct join user on user.id = buy.idUser where user.id in ( SELECT DISTINCT node as followers FROM tree) GROUP by product.id', res);
     }
     else if (mode === "NoSQL") {
-        const query = `MATCH (n:Person) RETURN n LIMIT 25`;
+        const query = `MATCH (p1) WHERE id(p1) = ` + req.body.userId + ` MATCH path = (p1)<-[:FOLLOW*1..` + req.body.niveau + `]-(p:Person) WITH distinct p as p MATCH (p)-[:BUY]-(r:Product) WITH count(r) as nbrProduct, r RETURN r.refProd as firstName, nbrProduct as total;`;
         const params = {};
         await nosqlManager.requestNoSQL(query, params, res);
     }
@@ -51,7 +51,7 @@ app.post('/ProductListFollowersWithSpecificProduct', async (req, res) => {
         await sqlManager.requestMySQL('WITH recursive tree(node, depth) AS ( SELECT link.idFollower AS node, 1 AS depth FROM link WHERE link.idInflu = ' + req.body.userId + ' UNION ALL SELECT link.idFollower, depth + 1 FROM link JOIN tree ON tree.node = link.idInflu JOIN buy ON buy.idUser = link.idInflu WHERE depth <= ' + req.body.niveau + ') SELECT product.id, product.ref, COUNT(*) AS achats FROM product JOIN buy ON product.id = buy.idProduct JOIN USER ON USER.id = buy.idUser WHERE USER.id IN ( SELECT DISTINCT node AS followers FROM tree ) AND product.id = ' + req.body.productId, res);
     }
     else if (mode === "NoSQL") {
-        const query = `MATCH (n:Person) RETURN n LIMIT 25`;
+        const query = `MATCH (p1) WHERE id(p1) = ` + req.body.userId + ` MATCH path = (p1)<-[:FOLLOW*1..` + req.body.niveau + `]-(p:Person) WITH distinct p as p MATCH (p)-[:BUY]-(r:Product) WHERE id(r) = "`+ req.body.productId +`" WITH count(r) as nbrProduct, r RETURN r.refProd as firstName, nbrProduct as total;`;
         const params = {};
         await nosqlManager.requestNoSQL(query, params, res);
     }
@@ -62,7 +62,7 @@ app.post('/CommandListByInfluence', async (req, res) => {
         await sqlManager.requestMySQL('WITH RECURSIVE followersthatbought(USER, depth, parent) AS ( SELECT buy.idUser, 0, 0 FROM buy WHERE buy.idProduct = ' + req.body.productId + ' UNION ALL SELECT link.idInflu, depth + 1, USER FROM followersthatbought, link, buy WHERE USER = link.idFollower AND buy.idProduct = ' + req.body.productId + ' AND buy.idUser = link.idInflu AND link.idInflu != parent AND link.idFollower != parent AND depth < ' + req.body.niveau + ' ) SELECT depth, COUNT(USER) AS nbAchat FROM ( SELECT DISTINCT USER,  depth FROM followersthatbought ) AS ud GROUP BY depth', res);
     }
     else if (mode === "NoSQL") {
-        const query = `MATCH (n:Person) RETURN n LIMIT 25`;
+        const query = `MATCH (p:Person)-[:BUY]->(ref:Product) WHERE id(ref) = ` + req.body.productId + ` WITH distinct(p) as p CALL apoc.neighbors.athop(p, "<FOLLOW",` + req.body.niveau + `) YIELD node as result MATCH (result)-[:BUY]->(ref:Product) WHERE id(ref)=` + req.body.productId + `RETURN count(distinct(result));`;
         const params = {};
         await nosqlManager.requestNoSQL(query, params, res);
     }
--- nosql/manager.js
+++ nosql/manager.js
@@ -39,103 +39,140 @@ const requestNoSQL = async function (query, params, res) {
 }
 
 const createNoSQL = async function(){
-    let pre_query = new Date().getTime();
     const session = driver.session();
     const params = {
     };
     let users = [];
     let products = [];
-    let queryBuilderConstraintProduct = `CREATE INDEX person_id_index IF NOT EXISTS FOR (p:Person) ON (p.idPers)`;
-    await session.run(queryBuilderConstraintProduct, params);
-    queryBuilderConstraintProduct = `CREATE INDEX product_id_index IF NOT EXISTS FOR (p:Product) ON (p.idProd)`;
-    await session.run(queryBuilderConstraintProduct, params);
-    let nbrInsertProd = 100;
+    let nbrInsertProd = 10000;
+    let allFollow = [];
+    let allBuy = [];
+    const nbRelationMax = 20;
     for(let j=0; j<nbrInsertProd; j++){
         let prod = {};
         prod.refProd = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 15);
         prod.idProd = j;
         products.push(prod);
-        let queryBuilderProduct = `CREATE (p: Product {idProd: ` + prod.idProd + `, reference: \"`+ prod.refProd + `\"})`;
-        const params = {
-        };
-        await session.run(queryBuilderProduct, params);
     }
-    let transaction;
-    let nbrInsert = 1000;
-    transaction = session.beginTransaction();
+    let productsMapped = products.map((product) => ({
+        idProd: product.idProd, refProd: product.refProd
+    }));
+    let nbrInsert = 1000000;
     for(let i=0; i < nbrInsert; i++){
-        if(i%1000==0){
-            await transaction.commit();
-            transaction = session.beginTransaction();
-        }
         let user = {};
         user.idPers = i;
         user.firstName = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 15);
         user.lastName = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 15);
-        user.products = [];
-        user.followers = [];
         users.push(user);
-        let queryBuilder =
-            `CREATE (p: Person {idPers: ` +  user.idPers + `, firstName: \"`+ user.firstName + `\" ,lastName: \"`+ user.firstName + `\"}) `;
-        const params = {
-        };
-        await transaction.run(queryBuilder, params);
     }
-    await transaction.commit();
-    for(let h=0; h<users.length; h++){
-        let nbrBuy = Math.floor(Math.random() * 6);
-        for(let k=0; k<nbrBuy; k++){
-            let product = Math.floor(Math.random() * nbrInsertProd);
-            users[h].products.push(products[product].idProd);
-        }
-        let nbrFollowers = Math.floor(Math.random() * 21);
-        for(let l=0; l<nbrFollowers; l++){
-            let u = h;
-            while(u == h){
-                u = Math.floor(Math.random() * nbrInsert);
+    let personsMapped = users.map((user) => ({
+        idPers: user.idPers, firstname: user.firstName, lastname: user.lastName
+    }));
+    let pre_query = new Date().getTime();
+    console.log("Debut insert product");
+    await session.writeTransaction((tx) => {
+        tx.run(
+            'UNWIND $map AS map CREATE (p:Product) SET p = map',
+            {
+                map: productsMapped,
             }
-            users[h].followers.push(users[u].idPers);
-        }
-    }
-    for(let l=0; l<users.length; l++){
-        transaction = session.beginTransaction();
-        for(let m=0; m<users[l].followers.length; m++){
-            let queryBuilder =
-                `MATCH (p1:Person) WHERE p1.idPers=` + users[l].idPers +
-                ` MATCH (p2:Person) WHERE p2.idPers=` + users[l].followers[m] +
-                ` CREATE (p2)-[:FOLLOW]->(p1);`;
-            const params = {
-            };
-            await transaction.run(queryBuilder, params);
-        }
-        await transaction.commit();
-        transaction = session.beginTransaction();
-        for(let n=0; n<users[l].products.length; n++){
-            let queryBuilder = `MATCH (p1:Person) WHERE p1.idPers=` + users[l].idPers +
-                                ` MATCH (p2:Product) WHERE p2.idProd=` + users[l].products[n] +
-                                ` CREATE (p1)-[:BUY]->(p2);`;
-            const params = {
-            };
-            await transaction.run(queryBuilder, params);
-        }
-        await transaction.commit();
+        );
+    });
 
+    console.log("debut insert person");
+    await session.writeTransaction((tx) => {
+        tx.run(
+            'UNWIND $map AS map CREATE (p:Person) SET p = map',
+            {
+                map: personsMapped,
+            }
+        );
+    });
+    let query = `MATCH (n:Person) RETURN count(n)`;
+    let response = await session.run(query, {});
+    let jsonObject = JSON.parse(JSON.stringify(response));
+    const nbPersons = jsonObject['records'][0]["_fields"][0];
+
+    query = 'MATCH (n:Product) RETURN count(n)';
+    response = await session.run(query, {});
+    jsonObject = JSON.parse(JSON.stringify(response));
+    const nbProducts = jsonObject['records'][0]["_fields"][0];
+
+    query = `MATCH (n:Person) RETURN min(id(n))`;
+    response = await session.run(query, {});
+    jsonObject = JSON.parse(JSON.stringify(response));
+    const idMinPerson = jsonObject['records'][0]["_fields"][0];
+    query = 'MATCH (n:Product) RETURN min(id(n))';
+    response = await session.run(query, {});
+    jsonObject = JSON.parse(JSON.stringify(response));
+    const idMinProduct = jsonObject['records'][0]["_fields"][0];
+
+    let followersList = [];
+    let randomFollow;
+
+    for (let i = 0; i < nbPersons; i++) {
+        let randNbRelation = Math.floor(Math.random() * nbRelationMax);
+        for (let j = 0; j < randNbRelation; j++) {
+            do {
+                randomFollow = Math.floor(Math.random() * nbPersons) + idMinPerson;
+            } while (followersList.includes(randomFollow) || randomFollow === i + idMinPerson);
+
+            followersList.push(randomFollow);
+        }
+        allFollow.push({
+            userId: i + idMinPerson,
+            followers: followersList
+        })
+        followersList = []
     }
-    /*if(users.length > 20){
-        let queryBuilder = `MATCH (f:Person) WITH DISTINCT collect(f) as followers, range(0,20) as followersRange
-            MATCH (i:Person) WITH i, apoc.coll.randomItems(followers, apoc.coll.randomItem(followersRange)) as followers
-            FOREACH (follower in followers | CREATE (follower)-[:FOLLOW]->(i))`
-        await session.run(queryBuilder, params);
+    let buysList = [];
+    let randomOrder;
+
+    for (let i = 0; i < nbPersons; i++) {
+        let numberOfOrder = Math.floor(Math.random() * 5);
+        for (let j = 0; j < numberOfOrder; j++) {
+            do {
+                randomOrder = Math.floor(Math.random() * nbProducts) + idMinProduct;
+            } while (buysList.includes(randomOrder) || randomOrder === i + nbProducts);
+
+            buysList.push(randomOrder);
+        }
+        allBuy.push({
+            userId: i + idMinPerson,
+            buys: buysList
+        });
+        buysList = [];
     }
-    if(products.length > 20){
-        let queryBuilder = `MATCH (f:Product) WITH DISTINCT collect(f) as products, range(0,5) as productsRange
-            MATCH (i:Person) WITH i, apoc.coll.randomItems(products, apoc.coll.randomItem(productsRange)) as products
-            FOREACH (product in products | CREATE (product)<-[:BUY]-(i))`
-        await session.run(queryBuilder, params);
-    }*/
+    console.log("debut buy");
+    await session.writeTransaction((tx) => {
+        tx.run(
+            `UNWIND $buys as buys
+           MATCH (p1:Person) WHERE ID(p1) = buys.userId
+           UNWIND buys.buys as buy
+           MATCH (p2:Product) WHERE ID(p2) = buy
+           CREATE (p1)-[:BUY]->(p2)
+           RETURN p1,p2`,
+            {
+                buys: allBuy
+            });
+    });
+    console.log("debut follower");
+    await session.writeTransaction((tx) => {
+        tx.run(
+            `UNWIND $follows as follows
+         MATCH (p1:Person) WHERE ID(p1) = follows.userId
+         UNWIND follows.followers as follower
+         MATCH (p2:Person) WHERE ID(p2) = follower
+         CREATE (p1)<-[:FOLLOW]-(p2)
+         RETURN p1, p2`,
+            {
+                follows: allFollow
+            });
+    });
+
     let post_query = new Date().getTime();
-    // calculate the duration in seconds
     let duration = (post_query - pre_query) / 1000;
+    console.log("Fin des insertions");
     console.log(duration);
     await session.close();
 }
